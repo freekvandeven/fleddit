10a7ba673dcc4f0548767be1fa488bac81fa2464
Freek van de Ven <FJM.vandeVen@student.han.nl>
2023-09-10 15:23:51 +0200

    feat: cleanup project

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,18 +14,7 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   group('MyWidget', () {
     testWidgets('should display a string of text', (WidgetTester tester) async {
-      // Define a Widget
-      const myWidget = MaterialApp(
-        home: Scaffold(
-          body: Text('Hello'),
-        ),
-      );
-
-      // Build myWidget and trigger a frame.
-      await tester.pumpWidget(myWidget);
-
-      // Verify myWidget shows some text
-      expect(find.byType(Text), findsOneWidget);
+      expect(true, true);
     });
   });
 }
